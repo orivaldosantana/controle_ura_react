d327ac90666ec2ed12c1abb969816a9a98bad263
Orivaldo Santana Jr <orivaldo@gmail.com>
2021-09-12 15:42:21 -0300

    formatação da mensagem de estatus

--- src/components/MessageMQTT.js
+++ src/components/MessageMQTT.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
 export default function MessageMQTT(props) {
+    let formatedTime = -1;
+    let leftLineSensor = -1;
+    let rightLineSensor = -1;
+    let distance = -1; 
+    if ( props.msg ){ 
+        let messageTerms = props.msg.split(' ');
+        let seconds = parseInt(messageTerms[1]);
+        
+        let date = new Date(null);
+        date.setSeconds(seconds); 
+        formatedTime = date.toISOString().substr(11, 8);
+         
+        distance = messageTerms[2]; 
+        leftLineSensor = messageTerms[3]; 
+        rightLineSensor = messageTerms[4]; 
+    }
     return (
         <div>
-            <p> <strong>Sensor de Distância(cm): </strong> XXX  </p>
-            <p> <strong>Sensor de Linha Esquerdo: </strong> XXX  </p>
-            <p> <strong>Sensor de Linha Direito: </strong> XXX  </p>
-            <p> <strong>Tempo de Atividade(min): </strong> {props.msg}  </p>
+            <p> <strong>Sensor de Distância(cm): </strong> {distance}  </p>
+            <p> <strong>Sensor de Linha Esquerdo: </strong> {leftLineSensor}  </p>
+            <p> <strong>Sensor de Linha Direito: </strong> {rightLineSensor}  </p>
+            <p> <strong>Tempo de Atividade(min): </strong> {formatedTime}  </p>
         </div>
     );
 }
